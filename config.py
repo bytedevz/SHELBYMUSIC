e89c276d4b5bb2b9afe4c4266fda4f5f58399b5f
bytedevz <147808151+bytedevz@users.noreply.github.com>
2023-10-13 21:55:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,10 +30,6 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO",
-    "https://github.com/DAXXTEAM/DAXXMUSIC",
-)
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
